aff708f06c7f5ef9a7c3d936b6dd67ea462b1e96
Frouk3 <68140414+Frouk3@users.noreply.github.com>
2026-06-19 19:08:56 +0300

    Push 3.4 version. FUCK

--- MGRModLoader/ModLoader.h
+++ MGRModLoader/ModLoader.h
@@ -266,7 +266,7 @@ namespace Logger
 namespace Updater
 {
 	inline bool bEnabled = true;
-	inline constexpr double fCurrentVersion = 3.3;
+	inline constexpr double fCurrentVersion = 3.4;
 	inline double fLatestVersion = -1.0;
 	inline HANDLE hUpdateThread;
 	enum UpdateStatus : unsigned int
